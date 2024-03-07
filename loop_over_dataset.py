d1df5fa130d32252f9bc141d263b7382a8904540
schien <schien>
2024-03-07 01:08:36 -0500

    Implemented bev_from_pcl.

--- loop_over_dataset.py
+++ loop_over_dataset.py
@@ -83,7 +83,7 @@ np.random.seed(10) # make random values predictable
 
 exec_detection = ['bev_from_pcl']
 exec_tracking = [] # options are 'perform_tracking'
-exec_visualization = ['show_pcl'] # options are 'show_range_image', 'show_bev', 'show_pcl', 'show_labels_in_image', 'show_objects_and_labels_in_bev', 'show_objects_in_bev_labels_in_camera', 'show_tracks', 'show_detection_performance', 'make_tracking_movie'
+exec_visualization = [] # options are 'show_range_image', 'show_bev', 'show_pcl', 'show_labels_in_image', 'show_objects_and_labels_in_bev', 'show_objects_in_bev_labels_in_camera', 'show_tracks', 'show_detection_performance', 'make_tracking_movie'
 
 exec_list = make_exec_list(exec_detection, exec_tracking, exec_visualization)
 vis_pause_time = 0 # set pause time between frames in ms (0 = stop between frames until key is pressed)
@@ -133,13 +133,13 @@ while True:
             lidar_pcl = load_object_from_file(results_fullpath, data_filename, 'lidar_pcl', cnt_frame)
 
         # ## Compute lidar birds-eye view (bev)
-        # if 'bev_from_pcl' in exec_list:
-        #     print('computing birds-eye view from lidar pointcloud')
-        #     lidar_bev = pcl.bev_from_pcl(lidar_pcl, configs_det)
-        # else:
-        #     print('loading birds-eve view from result file')
-        #     lidar_bev = load_object_from_file(results_fullpath, data_filename, 'lidar_bev', cnt_frame)
-        #
+        if 'bev_from_pcl' in exec_list:
+            print('computing birds-eye view from lidar pointcloud')
+            lidar_bev = pcl.bev_from_pcl(lidar_pcl, configs_det)
+        else:
+            print('loading birds-eve view from result file')
+            lidar_bev = load_object_from_file(results_fullpath, data_filename, 'lidar_bev', cnt_frame)
+
         # ## 3D object detection
         # if (configs_det.use_labels_as_objects==True):
         #     print('using groundtruth labels as objects')
